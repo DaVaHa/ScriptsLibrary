4fcdb9c21f778a3fb73e84071e02c86aa7ba6da6
Daniel Van Hasselt <daniel.vanhasselt@jbc.be>
2019-01-30 12:27:59 +0100

    os+shutil

--- library_os.py
+++ library_os.py
@@ -16,3 +16,12 @@ os.listdir(path)
 
 # rename files
 os.rename(old_file_path_name, new_file_path_name)
+
+# search in directories #default=topdown
+os.walk(path)
+
+for dirpath, dirnames, filenames in os.walk("U:\\"):
+    for filename in [f for f in filenames if f.endswith(".py")]:
+        f = os.path.join(dirpath, filename)
+        print(f)
+
---
+++ library_shutil.py
@@ -0,0 +1,12 @@
+'''
+
+PYTHON LIBRARY : shutil
+
+'''
+
+
+
+from shutil import copyfile
+
+#copy file
+copyfile(src, dst)
