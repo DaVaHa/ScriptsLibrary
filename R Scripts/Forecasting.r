d8888737b026953db8917f8bbb68d74ba6754741
Daniel Van Hasselt <daniel.vanhasselt@jbc.be>
2020-03-31 19:01:56 +0200

    r scripts

---
+++ R Scripts/Forecasting.r
@@ -0,0 +1,9 @@
+###########################
+##                       ##
+##      FORECASTING      ##
+##                       ##
+###########################
+
+# install.packages("forecast")
+
+library("forecast")
